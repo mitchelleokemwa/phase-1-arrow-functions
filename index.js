02561270c5b30ba4db05ff0d9e8d3156293af9dc
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-06 15:05:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(number1 = 2000, number2 = 100) {
+  let result = number1/number2;
+    return result;
+}
+const square =(number1) => {
+    return number1 * number1;}
+square(2);
+
+const add = (number1, number2) => {
+    console.log(`Adding ${number1}`);
+    console.log(`Adding ${number2}`);
+    return number1 + number2;
+  }
+  sum(4,8); 
